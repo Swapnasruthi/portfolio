a319993e91e43e0214f0df48bd78fffeda014560
swapnasruthi <swapnasruthi2005@gmail.com>
2025-06-10 13:53:13 +0530

    dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,9 @@ import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    <div className="min-h-screen bg-background text-foreground ">
       {/* Theme Toggle */}
-      <ThemeToggle />
+      < ThemeToggle />
       {/* Background Effects */}
       <StarBackground />
 
